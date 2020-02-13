e7ad0b990eb50763cc845b673a6be3dcecdcf7b8
Sam Kidman <samk@localhost.localdomain>
2020-02-13 18:48:32 +1100

    flesh out ip address calcs spec

--- cypress/integration/acceptance/calculating_ip_subnet.spec.js
+++ cypress/integration/acceptance/calculating_ip_subnet.spec.js
@@ -17,12 +17,28 @@ describe("Calculating IP subnets", () => {
   context("when the ip address is valid", () => {
     before(() => {
       cy.testid("ip_address_input").clear();
-      cy.testid("ip_address_input").type("192.168.254.1/24");
+      cy.testid("ip_address_input").type("192.168.254.5/24");
     });
 
-    it("displays the ip address", () => {
-      cy.testid("ip_address_value").contains("192.168.254.1");
+    it.skip("displays the network address", () => {
+      cy.testid("network_address_value").contains("192.168.254.0");
     });
+
+    it.skip("displays the low address", () => {
+      cy.testid("low_address_value").contains("192.168.254.1")
+    })
+
+    it("displays the ip address", () => {
+      cy.testid("ip_address_value").contains("192.168.254.5");
+    });
+
+    it.skip("displays the high address", () => {
+      cy.testid("high_address_value").contains("192.168.254.254")
+    })
+
+    it.skip("displays the broadcast address", () => {
+      cy.testid("broadcast_address").contains("192.168.254.255")
+    })
 
     it("displays the subnet mask", () => {
       cy.testid("subnet_mask_value").contains("255.255.255.0");
